f307918158461845803f967cdb15d29292288715
Alexander Petrovskiy <uiautomation@hotmail.com>
2024-11-20 00:38:42 +0300

    done or almost done

--- kubernetes-intro/run.sh
+++ kubernetes-intro/run.sh
@@ -8,30 +8,34 @@ POD_NAME=intro
 cp namespace_mine.yaml namespace.yaml
 cp pod_mine.yaml pod.yaml
 
+# initializing minikube
 minikube delete
+minikube start
 
-minikube start # --driver=kvm2
-
+# loading manifests and setting the context
 kubectl apply -f namespace.yaml
 kubectl apply -f account.yaml
 kubectl apply -f pod.yaml
+kubectl config set-context --current --namespace="${NS}"
 
 sleep 1m
-#
-kubectl get pod "${POD_NAME}" -o wide --namespace "${NS}"
-# NAME    READY   STATUS    RESTARTS   AGE   IP           NODE       NOMINATED NODE   READINESS GATES
-# intro   1/1     Running   0          60s   10.244.0.2   minikube   <none>           <none>
-POD_IP=$(kubectl get pod "${POD_NAME}" -o wide --namespace "${NS}" -ojsonpath='{.status.podIP}')
-#
-kubectl port-forward "${POD_NAME}" -n "${NS}" "${PORT}:${PORT}" &
-#
-kubectl get po -n "${NS}"
-#
-kubectl cluster-info
-#
-kubectl exec "${POD_NAME}" -- curl -s "http://${POD_IP}:${PORT}"
-curl "http://localhost:${PORT}/${NS}/"
-curl "http://localhost:${PORT}/${NS}/init/"
-kubectl describe "pod/${POD_NAME}" --namespace="${NS}"
 
-kubectl delete pod "${POD_NAME}" --namespace "${NS}"
+# checking the pod visually
+kubectl get pod "${POD_NAME}" -o wide
+
+kubectl port-forward "${POD_NAME}" "${PORT}:${PORT}" &
+
+# checking status of the pod containers
+kubectl get events | grep 'Started container init'
+kubectl get events | grep 'Started container web'
+
+# loading the index.html file that was created in the /init folder
+# and is available in the /homework folder
+POD_IP=$(kubectl get pod "${POD_NAME}" -o wide -ojsonpath='{.status.podIP}')
+kubectl exec "${POD_NAME}" -- curl -s "http://${POD_IP}:${PORT}"
+kubectl cp "${POD_NAME}:/homework/index.html" ./index1.html
+
+kubectl delete pod "${POD_NAME}"
+
+kubectl get events | grep FailedPreStopHook
+kubectl get events | grep PreStopHook
