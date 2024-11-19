cf9120d1803716be03ccbbeff9f2692c2c1135a2
Alexander Petrovskiy <uiautomation@hotmail.com>
2024-11-20 01:31:51 +0300

    finished

--- kubernetes-intro/run.sh
+++ kubernetes-intro/run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-set -x
+set +x
 
 NS=homework
 PORT=8000
 POD_NAME=intro
+PAGE_FILE=index.html
+PASSED=PASSED
+FAILED=FAILED
+
+rm -f "${PAGE_FILE}"
 
 cp namespace_mine.yaml namespace.yaml
 cp pod_mine.yaml pod.yaml
@@ -13,6 +18,7 @@ minikube delete
 minikube start
 
 # loading manifests and setting the context
+echo "=== Loading manifests... ==="
 kubectl apply -f namespace.yaml
 kubectl apply -f account.yaml
 kubectl apply -f pod.yaml
@@ -23,19 +29,26 @@ sleep 1m
 # checking the pod visually
 kubectl get pod "${POD_NAME}" -o wide
 
+# seemingly does not work
 kubectl port-forward "${POD_NAME}" "${PORT}:${PORT}" &
 
 # checking status of the pod containers
-kubectl get events | grep 'Started container init'
-kubectl get events | grep 'Started container web'
+echo "=== Checking that containers have started... ==="
+INIT_STARTED=$(kubectl get events | grep 'Started container init')
+WEB_STARTED=$(kubectl get events | grep 'Started container web')
+[ ! -z "${INIT_STARTED}" ] && echo "init started - ${PASSED}" || echo "init started - ${FAILED}"
+[ ! -z "${WEB_STARTED}" ] && echo "web started - ${PASSED}" || echo "web started - ${FAILED}"
 
 # loading the index.html file that was created in the /init folder
 # and is available in the /homework folder
+echo "=== Checking availability of the pod and existense of ${PAGE_FILE}... ==="
 POD_IP=$(kubectl get pod "${POD_NAME}" -o wide -ojsonpath='{.status.podIP}')
 kubectl exec "${POD_NAME}" -- curl -s "http://${POD_IP}:${PORT}"
-kubectl cp "${POD_NAME}:/homework/index.html" ./index1.html
+kubectl cp "${POD_NAME}:homework/${PAGE_FILE}" "./${PAGE_FILE}"
+[ -e "${PAGE_FILE}" ] && echo "${PAGE_FILE} found - ${PASSED}" || echo "${PAGE_FILE} found - ${FAILED}"
 
 kubectl delete pod "${POD_NAME}"
 
-kubectl get events | grep FailedPreStopHook
+FAILED_PRE_STOP_HOOK=$(kubectl get events | grep FailedPreStopHook)
 kubectl get events | grep PreStopHook
+[ -z "${FAILED_PRE_STOP_HOOK}" ] && echo "preStop hook no failure - ${PASSED}" || echo "preStop hook no failure - ${FAILED}"
